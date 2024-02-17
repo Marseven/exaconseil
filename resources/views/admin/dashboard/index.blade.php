129cef0536f6f12e9584c19155120e5ed38b2824
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-02-17 03:36:05 +0100

    Debug Dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -264,25 +264,7 @@
                                 <!--end::Card body-->
                             </div>
                             <!--end::Card widget 20-->
-                            <!--begin::Card widget 7-->
-                            <div class="card card-flush h-md-50 mb-5 mb-xl-10">
-                                <!--begin::Header-->
-                                <div class="card-header pt-5">
-                                    <!--begin::Title-->
-                                    <div class="card-title d-flex flex-column">
-                                        <!--begin::Amount-->
-                                        <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">357</span>
-                                        <!--end::Amount-->
-                                        <!--begin::Subtitle-->
-                                        <span class="text-gray-400 pt-1 fw-semibold fs-6">Professionals</span>
-                                        <!--end::Subtitle-->
-                                    </div>
-                                    <!--end::Title-->
-                                </div>
-                                <!--end::Header-->
 
-                            </div>
-                            <!--end::Card widget 7-->
                         </div>
                         <!--end::Col-->
                         <!--begin::Col-->
@@ -323,25 +305,7 @@
                                 <!--end::Card body-->
                             </div>
                             <!--end::Card widget 20-->
-                            <!--begin::Card widget 7-->
-                            <div class="card card-flush h-md-50 mb-5 mb-xl-10">
-                                <!--begin::Header-->
-                                <div class="card-header pt-5">
-                                    <!--begin::Title-->
-                                    <div class="card-title d-flex flex-column">
-                                        <!--begin::Amount-->
-                                        <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">357</span>
-                                        <!--end::Amount-->
-                                        <!--begin::Subtitle-->
-                                        <span class="text-gray-400 pt-1 fw-semibold fs-6">Professionals</span>
-                                        <!--end::Subtitle-->
-                                    </div>
-                                    <!--end::Title-->
-                                </div>
-                                <!--end::Header-->
 
-                            </div>
-                            <!--end::Card widget 7-->
                         </div>
                         <!--end::Col-->
                         <!--begin::Col-->
@@ -382,25 +346,7 @@
                                 <!--end::Card body-->
                             </div>
                             <!--end::Card widget 20-->
-                            <!--begin::Card widget 7-->
-                            <div class="card card-flush h-md-50 mb-5 mb-xl-10">
-                                <!--begin::Header-->
-                                <div class="card-header pt-5">
-                                    <!--begin::Title-->
-                                    <div class="card-title d-flex flex-column">
-                                        <!--begin::Amount-->
-                                        <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">357</span>
-                                        <!--end::Amount-->
-                                        <!--begin::Subtitle-->
-                                        <span class="text-gray-400 pt-1 fw-semibold fs-6">Professionals</span>
-                                        <!--end::Subtitle-->
-                                    </div>
-                                    <!--end::Title-->
-                                </div>
-                                <!--end::Header-->
 
-                            </div>
-                            <!--end::Card widget 7-->
                         </div>
                         <!--end::Col-->
 
@@ -442,25 +388,7 @@
                                 <!--end::Card body-->
                             </div>
                             <!--end::Card widget 20-->
-                            <!--begin::Card widget 7-->
-                            <div class="card card-flush h-md-50 mb-5 mb-xl-10">
-                                <!--begin::Header-->
-                                <div class="card-header pt-5">
-                                    <!--begin::Title-->
-                                    <div class="card-title d-flex flex-column">
-                                        <!--begin::Amount-->
-                                        <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">357</span>
-                                        <!--end::Amount-->
-                                        <!--begin::Subtitle-->
-                                        <span class="text-gray-400 pt-1 fw-semibold fs-6">Professionals</span>
-                                        <!--end::Subtitle-->
-                                    </div>
-                                    <!--end::Title-->
-                                </div>
-                                <!--end::Header-->
 
-                            </div>
-                            <!--end::Card widget 7-->
                         </div>
                         <!--end::Col-->
 
