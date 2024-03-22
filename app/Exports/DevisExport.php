71202f038383cb15aff94a8cbd953bbcf15c28e0
Aristide Mebodo <mebodoaristide@yahoo.fr>
2024-03-22 19:58:14 +0100

    Debug 61

--- app/Exports/DevisExport.php
+++ app/Exports/DevisExport.php
@@ -9,10 +9,19 @@ use Maatwebsite\Excel\Concerns\FromView;
 
 class DevisExport implements FromView
 {
+    protected $begin;
+    protected $end;
+
+    public function __construct($begin, $end)
+    {
+        $this->begin = new \DateTime($begin);
+        $this->end = new \DateTime($end);
+        $this->end->modify('+1 day');
+    }
 
     public function view(): View
     {
-        $devis = Devis::all();
+        $devis = Devis::where('created_at', '>=', $this->begin->format('Y-m-d'))->where('created_at', '<=', $this->end->format('Y-m-d'))->get();
         return view('admin.devis.export', [
             'devis' => $devis,
         ]);
